20dc18fb7e458f0cf97c1710223106ed4beca40b
Gelin Nguedong <gelin.nguedong@outlook.com>
2024-06-01 20:59:01 +0200

    - Added GET list of CashCards with sorting and pagination

--- src/main/java/dev/gelin/cashcard/persistence/CashCardRepository.java
+++ src/main/java/dev/gelin/cashcard/persistence/CashCardRepository.java
@@ -2,6 +2,7 @@ package dev.gelin.cashcard.persistence;
 
 import dev.gelin.cashcard.model.CashCard;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 }
